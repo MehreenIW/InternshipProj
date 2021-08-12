4c488aff2d66dbeff96002f61d8240fce11c86cf
MehreenIW <88751762+MehreenIW@users.noreply.github.com>
2021-08-12 16:41:20 +1200

    Update Student.java

--- studentmanagement/Student.java
+++ studentmanagement/Student.java
@@ -1,10 +1,19 @@
 package InternProject.studentmanagement;
 
 public class Student {
+    
     protected int studentId;
     protected String firstName;
     protected String lastName;
     protected String[] courses;
+    
+    public Student(int id, String firstname, String lastname, String[] courses) {
+        {
+            this.studentId = id;
+            this.firstName = firstname;
+            this.lastName = lastname;
+            this.courses = courses;
+        }
 
     public void setStudentId(int studentId) {
         this.studentId = studentId;
@@ -38,13 +47,7 @@ public class Student {
         return courses;
     }
 
-    public Student(int id, String firstname, String lastname, String[] courses) {
-        {
-            this.studentId = id;
-            this.firstName = firstname;
-            this.lastName = lastname;
-            this.courses = courses;
-        }
+    
 
 
     }
