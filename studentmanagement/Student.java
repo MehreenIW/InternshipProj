d1077a1d46322e2855166f114f58919964ffaa33
MehreenIW <mehreen.manzoor@integration.works>
2021-08-12 16:32:55 +1200

    Adding getter and setter methods

--- studentmanagement/Student.java
+++ studentmanagement/Student.java
@@ -6,6 +6,37 @@ public class Student {
     protected String lastName;
     protected String[] courses;
 
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCourses(String[] courses) {
+        this.courses = courses;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String[] getCourses() {
+        return courses;
+    }
 
     public Student(int id, String firstname, String lastname, String[] courses) {
         {
